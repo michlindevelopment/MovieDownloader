18ca5259293b701294c8ace97bac8ac2fb844446
Dan Michlin <michlindevelopment@gmail.com>
2022-04-17 23:06:55 +0300

    Gapupu!

--- app/src/main/java/com/michlindev/moviedownloader/SplashFragment.kt
+++ app/src/main/java/com/michlindev/moviedownloader/SplashFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.michlindev.moviedownloader.databinding.SplashFragmentBinding
@@ -18,9 +19,9 @@ class SplashFragment : Fragment() {
         fun newInstance() = SplashFragment()
     }
 
-    private lateinit var viewModel: SplashViewModel
+    private val viewModel: SplashViewModel by activityViewModels()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         /*val binding = DataBindingUtil.inflate<SplashFragmentBinding>(
             inflater,R.layout.splash_fragment,container,false
@@ -33,15 +34,17 @@ class SplashFragment : Fragment() {
             findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
         }
 
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
         //view.findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
 
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
+    /*override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
         // TODO: Use the ViewModel
-    }
+    }*/
 
 }
