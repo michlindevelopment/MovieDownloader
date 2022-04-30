dc027309889340d12047bff5cd9ad74e86aa84dd
Dan Michlin <michlindevelopment@gmail.com>
2022-04-30 22:29:32 +0300

    File upload

--- app/src/main/java/com/michlindev/moviedownloader/FileManager.kt
+++ app/src/main/java/com/michlindev/moviedownloader/FileManager.kt
@@ -7,13 +7,15 @@ object FileManager {
     private const val RSS_HEADER =
         "<rss xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:content=\"http://purl.org/rss/1.0/modules/content/\" xmlns:atom=\"http://www.w3.org/2005/Atom\" version=\"2.0\">"
     private const val RSS_TITLE = "<title>Movies RSS</title>"
+    private const val FILE_NAME = "my1.rss"
+
 
     private val path = MovieDownloader.applicationContext().filesDir.path
 
     private fun writeToRssFile(fileContent: List<String?>) {
         try {
             val directory = File("$path/MoviesSync")
-            val file = File(directory, "my.rss")
+            val file = File(directory, FILE_NAME)
             val fOut = FileOutputStream(file)
             val osw = OutputStreamWriter(fOut)
             for (line in fileContent) {
@@ -29,7 +31,7 @@ object FileManager {
 
     fun createFile() {
         val directory = File("$path/MoviesSync")
-        val file = File(directory, "my.rss")
+        val file = File(directory, FILE_NAME)
         try {
             if (!directory.exists()) directory.mkdir()
             if (!file.exists()) {
@@ -46,7 +48,7 @@ object FileManager {
     fun readFromRssFile(): List<String> {
         val fileContent: MutableList<String> = ArrayList()
         val directory = File("$path/MoviesSync")
-        val file = File(directory, "my.rss")
+        val file = File(directory, FILE_NAME)
         try {
             if (!directory.exists()) directory.mkdir()
             if (!file.exists()) {
--- app/src/main/java/com/michlindev/moviedownloader/main/MovieListViewModel.kt
+++ app/src/main/java/com/michlindev/moviedownloader/main/MovieListViewModel.kt
@@ -1,6 +1,15 @@
 package com.michlindev.moviedownloader.main
 
+import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.google.firebase.storage.FirebaseStorage
+import com.michlindev.moviedownloader.DLog
+import com.michlindev.moviedownloader.FileManager
+import com.michlindev.moviedownloader.MovieDownloader
+import com.michlindev.moviedownloader.SingleLiveEvent
+import java.io.File
+import java.io.FileInputStream
 
 class MovieListViewModel : ViewModel() {
     //Check if file exist on sd
@@ -9,4 +18,52 @@ class MovieListViewModel : ViewModel() {
 
     //Add fixed data
     //Upload it to firebase
+
+    var createFile = SingleLiveEvent<Any>()
+    var uploadFile = SingleLiveEvent<Any>()
+
+    fun createFile() {
+        //createFile.call()
+        FileManager.createFile()
+    }
+
+    fun uploadFile() {
+
+        val storageReference = FirebaseStorage.getInstance().reference
+
+        val path = MovieDownloader.applicationContext().filesDir.path
+        val directory = File("$path/MoviesSync")
+        //val file = File(directory, "my1.rss")
+
+
+        val file1 = Uri.fromFile(File("$path/MoviesSync/my1.rss"))
+        val mountainsRef = storageReference.child("my2.rss")
+        val uploadTask = mountainsRef.putFile(file1)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+            DLog.d("Fail $it")
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
+            DLog.d("Success")
+
+        }.addOnProgressListener {
+            DLog.d("Prog")
+        }
+
+        /*val mountainsRef = storageReference.child("my2.rss")
+        val stream = FileInputStream(file)
+        val uploadTask = mountainsRef.putStream(stream)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+            DLog.d("Fail $it")
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
+            DLog.d("Success")
+        }.addOnProgressListener {
+            DLog.d("Prog")
+        }*/
+
+    }
 }
