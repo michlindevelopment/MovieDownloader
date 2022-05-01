c30b5703edcd1d43f12498d746e9f925f89d35c4
Dan Michlin <michlindevelopment@gmail.com>
2022-05-01 23:35:38 +0300

    Adding menu

--- app/src/main/java/com/michlindev/moviedownloader/main/MovieListFragment.kt
+++ app/src/main/java/com/michlindev/moviedownloader/main/MovieListFragment.kt
@@ -1,17 +1,12 @@
 package com.michlindev.moviedownloader.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
+import com.michlindev.moviedownloader.DLog
 import com.michlindev.moviedownloader.R
 import com.michlindev.moviedownloader.databinding.MovieListFragmentBinding
-import com.michlindev.moviedownloader.databinding.SplashFragmentBinding
-import com.michlindev.moviedownloader.splash.SplashViewModel
 
 class MovieListFragment : Fragment() {
 
@@ -24,17 +19,29 @@ class MovieListFragment : Fragment() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
+
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.app_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_clear_file -> DLog.d("A")
+            R.id.action_upload_file -> DLog.d("B")
+            R.id.app_bar_switch_id -> DLog.d("C")
+            else -> {}
+        }
+        return true
     }
 
 
 
-    /*override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.movie_list_fragment, container, false)
-    }*/
+
 
 
 
